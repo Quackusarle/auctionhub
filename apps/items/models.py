d9443f6b27c6ee9418b9ba3cae652beb50b05504
Quackusarle <tvhoangg05@gmail.com>
2025-03-17 19:54:14 +0700

    items api

--- apps/items/models.py
+++ apps/items/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    item_id = models.AutoField(primary_key=True)
+    seller_id = models.IntegerField()
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    image_url = models.CharField(max_length=255, blank=True, null=True)
+    starting_price = models.DecimalField(max_digits=10, decimal_places=2)
+    current_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    end_time = models.DateTimeField()
+    status = models.CharField(
+        max_length=10,
+        choices=[('ongoing', 'Ongoing'), ('completed', 'Completed'), ('canceled', 'Canceled')],
+        default='ongoing'
+    )
+
+    class Meta:
+        managed = False
+        db_table = 'items'
+
+    def __str__(self):
+        return self.name
--- apps/items/views.py
+++ apps/items/views.py
@@ -1,3 +1,54 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Item
+from .serializers import ItemSerializer
 
 # Create your views here.
+
+class ItemList(APIView):
+    def get(self, request):
+        items = Item.objects.all()
+        serializer = ItemSerializer(items, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ItemSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ItemDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Item.objects.get(pk=pk)
+        except Item.DoesNotExist:
+            return None
+
+    def get(self, request, pk):
+        item = self.get_object(pk)
+        if item:
+            serializer = ItemSerializer(item)
+            return Response(serializer.data)
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def put(self, request, pk):
+        item = self.get_object(pk)
+        if item:
+            serializer = ItemSerializer(item, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def delete(self, request, pk):
+        item = self.get_object(pk)
+        if item:
+            item.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+
