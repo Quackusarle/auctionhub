dc1df6087fa6dffeceae036b98a076dd1584c768
Quackusarle <tvhoangg05@gmail.com>
2025-03-19 19:44:28 +0700

    change field

--- apps/bidding/models.py
+++ apps/bidding/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from apps.auth_users.models import User
 from apps.items.models import Item
+from django.conf import settings
 
 class Bid(models.Model):
     bid_id = models.AutoField(primary_key=True)
     item_id = models.ForeignKey(Item, on_delete=models.CASCADE)  # Sản phẩm được đấu giá
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)  # Người đặt giá
+    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)  # Người đặt giá
     bid_amount = models.DecimalField(max_digits=10, decimal_places=2)  # Giá đấu thầu
     bid_time = models.DateTimeField(auto_now_add=True)  # Thời gian đặt giá
 
