92c6f417d1d6276f680c615c15a928a0fa72e1a5
huyvinh0605 <huyvinh199@gmail.com>
2025-03-30 22:04:49 +0700

    Chinh sua bidding

--- apps/bidding/serializers.py
+++ apps/bidding/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
 from .models import Bid
+from .models import Item
+from .models import User
+
 
 class Bidserializers(serializers.ModelSerializer):
+    item_id = serializers.PrimaryKeyRelatedField(queryset=Item.objects.all())
+    user_id = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
     class Meta:
-        models = Bid
-        fields = '__all__'
+        model = Bid
+        fields = "__all__"
     
--- apps/bidding/urls.py
+++ apps/bidding/urls.py
@@ -3,6 +3,6 @@ from .views import place_bid, get_bids_for_item, get_highest_bid
 
 urlpatterns = [
     path('place_bid/', place_bid, name='place_bid'),
-    path('get_bids/<int:item_id>/', get_bids_for_item, name='get_bids_for_item'),
-    path('highest_bid/<int:item_id>/',get_highest_bid, name='get_highest_bid'),
+    path('get_bids/', get_bids_for_item, name='get_bids_for_item'),
+    path('highest_bid/',get_highest_bid, name='get_highest_bid'),
 ]
