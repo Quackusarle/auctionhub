346eec6876201d8c62ec6859c86630ce202db078
Quackusarle <tvhoangg05@gmail.com>
2025-05-23 17:43:23 +0700

    api cho item list

--- apps/items/urls.py
+++ apps/items/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from .views import ItemList, ItemDetail, ItemCreate, ItemSearchAPI
+from .views import ItemList, ItemDetail, ItemCreate, ItemSearchAPI, ItemListAPI
 from . import views
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('items/', ItemList.as_view(), name='item-list'),
+    path('items/', ItemListAPI.as_view(), name='item-list'),
     path('items/create-auction/', ItemCreate.as_view(), name='create-auction'),
     path('items/<int:pk>/', ItemDetail.as_view(), name='item-detail'),
     path('items/search/', ItemSearchAPI.as_view(), name='item_search_api'),
-    
 ]
