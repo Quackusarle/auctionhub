ba1fcc1d8444c18a7f6c1860b2616c4932e1af12
X18112005 <dinhhungnguyenqt@gmail.com>
2025-05-26 21:37:52 +0700

    Chinh sua consumers

--- apps/items/consumers.py
+++ apps/items/consumers.py
@@ -1,15 +1,17 @@
 # consumers.py
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
 
 class HomeBidConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.group_name = "homepage_items"
         await self.channel_layer.group_add(self.group_name, self.channel_name)
         await self.accept()
-        print("✅ WebSocket kết nối với trang chủ")
+        print("WebSocket kết nối với trang chủ")
 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(self.group_name, self.channel_name)
-        print("❌ WebSocket trang chủ bị ngắt")
+        print("WebSocket trang chủ bị ngắt")
 
     async def bid_update(self, event):
         await self.send(text_data=json.dumps({
