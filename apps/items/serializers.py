9156107a1db6db39f123c5093d9290e97d834c2b
dathwang <datquochoang0587@gmail.com>
2025-05-28 22:50:45 +0700

    update item apps for cloudinary-image url

--- apps/items/serializers.py
+++ apps/items/serializers.py
@@ -2,11 +2,12 @@ from rest_framework import serializers
 from .models import Item # Đảm bảo model Item được import đúng
 
 class ItemSerializer(serializers.ModelSerializer):
+    image_url = serializers.CharField(required=False, allow_blank=True, allow_null=True)
+
     class Meta:
         model = Item
-        # Bỏ 'image_url' ra khỏi danh sách các trường
         # 'seller' vẫn là read_only và được gán trong view
         # 'current_price' và 'status' có giá trị default trong model
         # 'item_id' là AutoField
-        fields = ['item_id', 'name', 'description', 'starting_price', 'end_time', 'seller', 'current_price', 'status']
+        fields = ['item_id', 'name', 'description', 'starting_price', 'end_time', 'seller', 'current_price', 'status', 'image_url']
         read_only_fields = ['seller', 'item_id', 'current_price', 'status']
